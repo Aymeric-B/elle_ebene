134b9acbd925571b434c5f52553fd47a36c43378
Louis Contamin <louis@contamin.net>
2021-06-29 12:21:41 +0200

    Trainer pour Aymeric

---
+++ elle_ebene/params.py
@@ -0,0 +1,10 @@
+#params.py
+#Fichier  de paramètres
+
+# Experiment name for MLFolow system
+EXPERIMENT_NAME = "[FR] [Lyon] [myr-aym-lou] elle_ebene_baseline V0"
+
+# GCP parameters
+BUCKET_NAME = "elle_ebene_bucket"
+MODEL_NAME = "Baseline"
+MODEL_VERSION = "V0"
---
+++ elle_ebene/trainer.py
@@ -0,0 +1,81 @@
+# trainer.py
+
+from params import *
+import joblib
+from termcolor import colored
+from memoized_property import memoized_property
+from mlflow.tracking import MlflowClient
+from elle_ebene.gcp import storage_upload
+
+
+class Trainer(object):
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+        # for MLFlow
+        self.experiment_name = EXPERIMENT_NAME
+
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
+
+    def set_pipeline(self):
+        '''To be defined'''
+
+
+    def run(self):
+        self.set_pipeline()
+        self.mlflow_log_param("model", "Linear")
+        self.pipeline.fit(self.X, self.y)
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the accuracy"""
+
+    def save_model_locally(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+
+    # MLFlow methods
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+if __name__ == "__main__":
+    # Get and clean data
+    y = 
+    X = 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Train and save model, locally and
+    trainer = Trainer(X=X_train, y=y_train)
+    trainer.set_experiment_name(experiment_name)
+    trainer.run()
+    accuracy = trainer.evaluate(X_test, y_test)
+    print(f"accuracy: {accuracy}")
+    trainer.save_model_locally()
+    storage_upload()
