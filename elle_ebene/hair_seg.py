f98ac88f0d6ea9adeeabfde7dbb78dc7af3c1642
aymeric boffa <aymeric.boffa@gmail.com>
2021-07-01 14:17:25 +0200

    added seg.py

--- elle_ebene/hair_seg.py
+++ elle_ebene/hair_seg.py
@@ -16,7 +16,7 @@ class HairSegmenter():
         self.model = None
         
 
-    def model_init(self, path = "model_weights/hair_seg/weights"):
+    def model_init(self, path = "raw_data/hair_seg/weights"):
         """
         Initialize the model
         by default path = relative path for the website
