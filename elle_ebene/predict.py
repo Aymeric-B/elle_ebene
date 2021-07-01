6001614e196b8f2ce7bb085c29e4c725fe701bb1
aymeric boffa <aymeric.boffa@gmail.com>
2021-07-01 09:14:32 +0200

    modified predict: now a Class

--- elle_ebene/predict.py
+++ elle_ebene/predict.py
@@ -4,16 +4,29 @@ from elle_ebene.simple_preprocessing import resize_img, to_numpy_rgb, squared_im
 from elle_ebene.params import RESIZING_DIM
 import numpy as np
 
-def baseline(img):
-    """
-    prends en entrée une image sous format PIL Image
-    """
-    model = initialize_model()
+class Predict():
+    def __init__(self, model_type = "baseline"):
+        """
+            model_type = nom du modèle choisi, par défault 'baseline'
+        """
+        self.pipeline = None
+        self.model_type = model_type
 
-    model.load_weights("../model_weights/baseline/weights")
+    def model_init(self):
+        """
+        Initialize the model
+        """
+        if self.model_type == "baseline":
+            self.pipeline = initialize_model()
+            self.pipeline.load_weights("../model_weights/baseline/weights")
 
 
-    clean_image = normalize(np.asarray(squared_imgs(to_numpy_rgb([resize_img(img, RESIZING_DIM)]))))
-    prediction = np.argmax(model.predict(clean_image), axis = -1)[0]
+    def predict(self, img):
+        """
+        prends en entrée une image sous format PIL Image
+        """
 
-    return prediction
+        clean_image = normalize(np.asarray(squared_imgs(to_numpy_rgb([resize_img(img, RESIZING_DIM)]))))
+        prediction = np.argmax(self.pipeline.predict(clean_image), axis = -1)[0]
+
+        return prediction
