c8c32fef2295a871efdbc6e0c095e9c4e64919a8
Myriam Genety <mygenety@gmail.com>
2021-07-05 14:49:27 +0200

    the new and improved class data aug

--- elle_ebene/dataug_inutile.py
+++ elle_ebene/dataug_inutile.py
@@ -5,12 +5,11 @@ from elle_ebene.utils.simple_preprocessing import to_numpy_rgb, squared_imgs, ge
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-
 # Home directory
 home_path = r'/Users/Mygenety/code/Aymeric-B/elle_ebene/raw_data/Data'
 path_pic = '/Users/Mygenety/code/Aymeric-B/elle_ebene/raw_data/Data_aug'
 # Variables
-batch_size = 132
+batch_size = 64
 
 # Preprocessing image 
 type3_imgs = get_images("Type 3", path=home_path)
@@ -22,28 +21,13 @@ type4_imgs_rgb = to_numpy_rgb(type4_imgs)
 type3_imgs_squared = squared_imgs(type3_imgs_rgb)
 type4_imgs_squared = squared_imgs(type4_imgs_rgb)
 
-def data_gen(path_pic, batch_size):
+def data_gen(path_pic, batch_size, model):
 #Generate batches of tensor image data with real-time data augmentation.
 #The data will be looped over (in batches so the size should be choosen in according to dataset size)
     train_path = os.path.join(path_pic,'train')
     val_path = os.path.join(path_pic,'val')
     
-    #count the files   
-    def folder_count(directory):
-        APP_FOLDER = directory
-        totalFiles =0
-        for base, dirs, files in os.walk(APP_FOLDER):
-            files = 0
-            #totalDir = 0
-            print('Searching in : ',base)
-            for Files in files:
-                files +=1
-                totalFiles += 1
-            print('Total number of files',totalFiles)
-        return totalFiles
-    
-    batch_size=folder_count(train_path)
-    
+# Data augmentation to training data   
     datagen = ImageDataGenerator(
         rotation_range=30,
         width_shift_range=0.2,
@@ -52,31 +36,58 @@ def data_gen(path_pic, batch_size):
         fill_mode='constant',
         cval = 255.,
         brightness_range=[0.6,1.8]) 
-    
+# No data augmentation to validation data   
     valid_datagen = ImageDataGenerator()
 
 # ImageDataGenerator flow_from_directory pulls files one after another from a specific directory
     train_generator = datagen.flow_from_directory(
-        directory=train_path, # get images from train folder
+        directory=train_path, 
         color_mode="rgb", 
         batch_size=batch_size,  # number of images to extract from folder for every batch
         class_mode="binary",
-        save_to_dir= r'/Users/Mygenety/code/Aymeric-B/elle_ebene/raw_data/augmented')    # classes to predict
+        save_to_dir= r'/Users/Mygenety/code/Aymeric-B/elle_ebene/raw_data/augmented')    # keep if you want to store images
     
     train_generator.next()
     
     validation_generator = valid_datagen.flow_from_directory(
-        directory=val_path, # same directory as training data
+        directory=val_path, 
         color_mode="rgb", 
         batch_size=batch_size,
         class_mode='binary')
+
+# training the model   
+    total_images = train_generator.n  
+    steps = total_images//batch_size    
+    
+    model.fit_generator(  #vs a .fit because we have data augmentation
+        train_generator,
+        steps_per_epoch=steps, # where is the train data?
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800)
     
     return train_generator, validation_generator
 # After you have created and configured your ImageDataGenerator, you must fit it on your data. 
 # This will calculate any statistics required to actually perform the transforms to your image data. 
 # You can do this by calling the fit() function on the data generator and pass it your training dataset.
 
-
+    # #count the files   
+    # def folder_count(directory):
+    #     APP_FOLDER = directory
+    #     #totalFiles =0
+    #     for base, dirs, files in os.walk(APP_FOLDER):
+    #         files = 0
+    #         #totalDir = 0
+    #         print('Searching in: ', base)
+    #         print('Number of directories:' , dirs)
+    #         for Files in files:
+    #             files +=1
+    #             #totalFiles += 1
+    #         print('Total number of files', files)
+    #     return  files
+    
+    # batch_size=folder_count(train_path)
+    # print(batch_size)
  
    
 
