cc3c33227420348e8b9e8c23443c0b4d6a858d54
aymeric boffa <aymeric.boffa@gmail.com>
2021-07-06 17:39:00 +0200

    corrected rpedict

--- elle_ebene/predict.py
+++ elle_ebene/predict.py
@@ -27,7 +27,7 @@ class Predict():
         if MODEL_USED == "baseline":
             self.pipeline = initialize_model_base()
         elif MODEL_USED == "seg_aug":
-            self.pipeline = initialize_model1()
+            self.pipeline = initialize_model_1()
 
         self.pipeline.load_weights(path)
 
